b85d575280c5ce934645351eac89c6e797a2d80a
kjsuarez <kjsuarez94@gmail.com>
2018-10-15 16:48:50 -0400

    successfully recieve post request and return parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
   def hello
-    render json: {json: 'from rails'}
+    render json: {json: 'rails get response'}
+  end
+
+  def poster
+    puts("params:")
+    puts(params[:_json])
+    render json: {json: "rails post response with params #{params[:_json]}"}
   end
 end
